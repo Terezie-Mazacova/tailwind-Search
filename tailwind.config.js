b4fbb3ca2feaf0927dd3ef6aa51d2d3357d97300
Terezie Mazáčová <terezkamazacova@post.cz>
2024-08-16 01:10:14 +0200

    skoro hotovy projekt Search (zbyva mobile first)

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      boxShadow: {
+        'custom-shadow': '0 4px 4px 0 rgba(46, 26, 81, 0.25)',
+      },
+
+      colors: {
+        'lavender-gray' : '#DBD5EC',
+        'indigo' : '#281A51',
+        'soft-pink' : '#FFECEC',
+        'pastel-lavender' : '#F7ECFF',
+        'soft-blue' : '#ECF0FF',
+        'mint-cream' : '#D7F9F3',
+        'fresh-lime' : '#E2FFD4',
+        'vanilla' : '#FFF3C9',
+        'creamy-peach' : '#FFD9C9',
+      },
+    },
+  },
+  plugins: [],
+}
